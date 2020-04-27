c9e803f74345533572a06725d79df1c1a2018f89
sacc <50913285+filipkmiecik@users.noreply.github.com>
2020-04-27 19:07:28 +0200

    updated sidebar

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
-import Sidebar from './Sidebar';
+import React, { Component } from "react";
+import Sidebar from "./Sidebar";
 
 export default class ChatContainer extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             chats: [],
-             activeChat: null,
-        }
-    }
+  constructor(props) {
+    super(props);
 
-    setActiveChat = (activeChat) => {
-        this.setState({activeChat})
-    }
-    
-    render() {
-        const {user, logout} = this.props;
-        return (
-            <div className="container">
-                <Sidebar 
-                logout={logout}
-                //chats={chats}
-                user={user}
-                //activeChat={activeChat}
-                setActiveChat={this.setActiveChat}
-                />
-            </div>
-        )
-    }
+    this.state = {
+      chats: [],
+      activeChat: null,
+    };
+  }
+
+  setActiveChat = (activeChat) => {
+    this.setState({ activeChat });
+  };
+
+  render() {
+    const { user, logout } = this.props;
+    return (
+      <div className="container">
+        <Sidebar
+          logout={logout}
+          user={user}
+          setActiveChat={this.setActiveChat}
+        />
+      </div>
+    );
+  }
 }
